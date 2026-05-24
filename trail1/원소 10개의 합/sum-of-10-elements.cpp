f08b9db657afc359895f082c1e657610c650d01d
looksambrook <104709953+looksambrook@users.noreply.github.com>
2026-05-24 20:48:13 +0900

    [Passed] 원소 10개의 합 (2ms, 0MB)

---
+++ trail1/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int sum=0;
+    for(int i=0;i<10;++i){
+        int tmp;
+        cin>>tmp;
+        sum+=tmp;
+    }
+    cout<<sum;
+    return 0;
+}
