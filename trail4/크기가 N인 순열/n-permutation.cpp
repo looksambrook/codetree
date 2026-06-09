9e7727329ffb9f663f1dfa1a203e9d1e32ba5d03
looksambrook <104709953+looksambrook@users.noreply.github.com>
2026-06-09 23:08:48 +0900

    [Passed] 크기가 N인 순열 (38ms, 1MB)

---
+++ trail4/크기가 N인 순열/n-permutation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[8]={0,};
+bool visited[8]={false,};
+
+void DFS(int x){
+    if(x==n){
+        for(int i=0;i<n;++i){
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+    for(int i=1;i<=n;++i){
+        if(visited[i-1])continue;
+        visited[i-1]=true;
+        arr[x]=i;
+        DFS(x+1);
+        visited[i-1]=false;
+    }
+}
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    DFS(0);
+
+    return 0;
+}
