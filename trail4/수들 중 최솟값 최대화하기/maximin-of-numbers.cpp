ade220e7d3a772a018ac325bb4471cf4ccaa4b02
looksambrook <104709953+looksambrook@users.noreply.github.com>
2026-06-10 00:11:42 +0900

    [Passed] 수들 중 최솟값 최대화하기 (18ms, 0MB)

---
+++ trail4/수들 중 최솟값 최대화하기/maximin-of-numbers.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[10][10];
+int ans=0;
+bool visited[10]={false, };
+
+void DFS(int x, int tmp_ans){
+    if(x==n){
+        ans=ans<tmp_ans?tmp_ans:ans;
+        return;
+    }
+    for(int i=0;i<n;++i){
+        if(visited[i])continue;
+        visited[i]=true;
+        int tmp=tmp_ans>grid[x][i]?grid[x][i]:tmp_ans;
+        DFS(x+1,tmp);
+        visited[i]=false;
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+    DFS(0, 10000);
+    cout<<ans;
+
+    return 0;
+}
