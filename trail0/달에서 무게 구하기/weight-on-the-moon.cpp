84355c020714e470878ba55fe47acb02229f9d46
looksambrook <104709953+looksambrook@users.noreply.github.com>
2026-06-24 21:13:37 +0900

    [Passed] 달에서 무게 구하기 (2ms, 0MB)

---
+++ trail0/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout<<fixed;
+    cout.precision(6);
+    int weight=13;
+    float rate=0.165;
+    cout<<weight<<" * "<<rate<<" = "<<weight*rate;
+    return 0;
+}
