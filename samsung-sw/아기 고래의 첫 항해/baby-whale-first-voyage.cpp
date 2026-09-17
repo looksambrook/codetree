79f44452605cc79652325102defa8edb57244629
looksambrook <104709953+looksambrook@users.noreply.github.com>
2026-09-18 06:26:26 +0900

    [Passed] 아기 고래의 첫 항해 (3ms, 0MB)

--- samsung-sw/아기 고래의 첫 항해/baby-whale-first-voyage.cpp
+++ samsung-sw/아기 고래의 첫 항해/baby-whale-first-voyage.cpp
@@ -1,131 +1,112 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
-const int MAX_N = 50;
+bool board[50][50] = { false, };
+bool visited[50][50] = { false, };
+int N, r, c, d;
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
 
-// 상, 우, 하, 좌
-const int dr[4] = {-1, 0, 1, 0};
-const int dc[4] = {0, 1, 0, -1};
-
-// 직진, 좌회전, 우회전, 뒤돌기
-const int turnOrder[4] = {0, 3, 1, 2};
-
-// BFS 경로 우선순위: 좌, 하, 우, 상
-const int searchOrder[4] = {3, 2, 1, 0};
-
-struct State {
+struct Info
+{
     int r;
     int c;
-    int dir;
+    int d;
+
+    /*bool operation(const Info& other) {
+        return r < other.r
+    }*/
 };
 
-int n;
-int board[MAX_N][MAX_N];
-bool visited[MAX_N][MAX_N];
-
-bool canMove(int r, int c) {
-    return 0 <= r && r < n &&
-           0 <= c && c < n &&
-           board[r][c] == 0;
+bool is_range(int x, int y) {
+    return x >= 0 && x < N
+        && y >= 0 && y < N
+        && !board[x][y];
 }
 
-// 1단계: 인접한 미방문 바다로 이동
-bool moveAdjacent(State& whale) {
+bool first_step() {
+    int df[4][4] = { 0,2,3,1,1,3,2,0,2,1,0,3,3,0,1,2 };
     for (int i = 0; i < 4; ++i) {
-        int nd = (whale.dir + turnOrder[i]) % 4;
-        int nr = whale.r + dr[nd];
-        int nc = whale.c + dc[nd];
-
-        if (!canMove(nr, nc) || visited[nr][nc]) continue;
-
-        whale = {nr, nc, nd};
-        return true;
+        int nd = df[d][i];
+        int nx = r + dx[nd];
+        int ny = c + dy[nd];
+        if (is_range(nx, ny) && !visited[nx][ny] && !board[nx][ny]) {
+            r = nx, c = ny, d = nd;
+            visited[r][c] = true;
+            return true;
+        }
     }
-
     return false;
 }
 
-// 2단계: 다음 목적지와 도착 방향 반환
-State findNext(const State& whale) {
-    bool searched[MAX_N][MAX_N] = {};
-    queue<State> q;
+bool second_step() {
+    int ds[4] = { 2,1,3,0 };
+    queue<Info> q;
+    queue<Info> ans;
+    q.push({ r,c,d });
+    bool selected[50][50] = { false, };
+    selected[r][c] = true;
 
-    q.push(whale);
-    searched[whale.r][whale.c] = true;
-
-    while (!q.empty()) {
-        int levelSize = static_cast<int>(q.size());
-        State target = {-1, -1, -1};
-
-        // 현재 큐에 있는 같은 거리의 칸들을 모두 처리
-        for (int i = 0; i < levelSize; ++i) {
-            State current = q.front();
+    while (ans.empty()) {
+        int cnt = q.size();
+        if (cnt == 0)return false;
+        for (int j = 0; j < cnt; ++j) {
+            int cx = q.front().r;
+            int cy = q.front().c;
+            int cd = q.front().d;
             q.pop();
 
-            for (int j = 0; j < 4; ++j) {
-                int nd = searchOrder[j];
-                int nr = current.r + dr[nd];
-                int nc = current.c + dc[nd];
-
-                if (!canMove(nr, nc) || searched[nr][nc]) continue;
-                searched[nr][nc] = true;
-
-                State next = {nr, nc, nd};
-
-                if (!visited[nr][nc]) {
-                    // 같은 거리의 후보 중 행, 열이 작은 칸 선택
-                    if (target.r == -1 ||
-                        nr < target.r ||
-                        (nr == target.r && nc < target.c)) {
-                        target = next;
+            for (int i = 0; i < 4; ++i) {
+                int nd = ds[i];
+                int nx = cx + dx[nd];
+                int ny = cy + dy[nd];
+                if (is_range(nx, ny) && !selected[nx][ny] && !board[nx][ny]) {
+                    selected[nx][ny] = true;
+                    if (visited[nx][ny])q.push({ nx,ny,nd });
+                    else {
+                        ans.push({ nx,ny,nd });
                     }
-                } else {
-                    // 이미 방문한 바다는 통과 가능
-                    q.push(next);
                 }
             }
         }
-
-        // 같은 거리의 후보를 모두 비교한 뒤 종료
-        if (target.r != -1) return target;
     }
 
-    return {-1, -1, -1};
+    r = ans.front().r;
+    c = ans.front().c;
+    d = ans.front().d;
+    ans.pop();
+
+    while (!ans.empty()) {
+        if ((r > ans.front().r)||(r==ans.front().r&&c>ans.front().c)) {
+            r = ans.front().r;
+            c = ans.front().c;
+            d = ans.front().d;
+        }
+        ans.pop();
+    }
+    visited[r][c] = true;
+    return true;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    // Please write your code here.
+    cin >> N >> r >> c >> d;
+    r -= 1, c -= 1, d -= 1;
+    visited[r][c] = true;
 
-    int r, c, inputDir;
-    cin >> n >> r >> c >> inputDir;
-
-    // 입력: 1=상, 2=하, 3=좌, 4=우
-    // 내부: 0=상, 1=우, 2=하, 3=좌
-    const int dirMap[5] = {0, 0, 2, 3, 1};
-    State whale = {r - 1, c - 1, dirMap[inputDir]};
-
-    int remaining = 0;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
             cin >> board[i][j];
-            if (board[i][j] == 0) ++remaining;
         }
     }
 
-    while (remaining > 0) {
-        visited[whale.r][whale.c] = true;
-        cout << whale.r + 1 << ' ' << whale.c + 1 << '\n';
-
-        --remaining;
-        if (remaining == 0) break;
-
-        if (moveAdjacent(whale)) continue;
-
-        whale = findNext(whale);
-        if (whale.r == -1) break;
+    while (true) {
+        cout << r + 1 << " " << c + 1 << "\n";
+        if (first_step())continue;
+        if (second_step())continue;
+        break;
     }
 
     return 0;
