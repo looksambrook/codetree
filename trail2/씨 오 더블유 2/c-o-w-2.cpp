8fd952858336abf61d3e837b88402aaac71441b2
looksambrook <104709953+looksambrook@users.noreply.github.com>
2026-09-25 01:12:15 +0900

    [Passed] 씨 오 더블유 2 (2ms, 0MB)

---
+++ trail2/씨 오 더블유 2/c-o-w-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+string str;
+
+int main() {
+    cin >> n;
+    cin >> str;
+
+    // Please write your code here.
+    int ans=0;
+    for(int i=0;i<n-2;++i){
+        if(str[i]!='C')continue;
+        for(int j=i+1;j<n-1;++j){
+            if(str[j]!='O')continue;
+            for(int k=j+1;k<n;++k){
+                if(str[k]!='W')continue;
+                ans++;
+            }
+        }
+    }
+    cout<<ans;
+
+    return 0;
+}
